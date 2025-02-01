d3c4069beec4444c4604c41051038d640f628afb
YFliXeR <yousefxzed@gmail.com>
2025-02-01 22:17:52 +0200

    Cleaning the code a bit

--- core/__init__.py
+++ core/__init__.py
@@ -1,11 +1,11 @@
-# this lstm core module implementation provides an implementation
-# of time series prediction using a lstm approach. It is provided
-# as is with no warranties or support.
-
+"""
+This LSTM core module provides an implementation of time series prediction using an LSTM approach.
+It is provided as-is with no warranties or support.
+"""
 __author__ = "Jakob Aungiers"
 __copyright__ = "Jakob Aungiers 2018"
 __version__ = "2.0.0"
 __license__ = "MIT"
 
 import warnings
-warnings.filterwarnings("ignore") # ignore messy numpy warnings
+warnings.filterwarnings("ignore") # Ignore messy NumPy warnings
--- core/utils.py
+++ core/utils.py
@@ -1,13 +1,19 @@
 import datetime as dt
 
-class Timer():
+class Timer:
+	#A simple timer class to measure elapsed time.
 
 	def __init__(self):
 		self.start_dt = None
 
 	def start(self):
+		#Start the timer
 		self.start_dt = dt.datetime.now()
 
 	def stop(self):
+		#Stop the timer and print the elapsed time
+		if self.start_dt is None:
+			raise ValueError("Timer was not started. Call start() before stop().")
 		end_dt = dt.datetime.now()
-		print('Time taken: %s' % (end_dt - self.start_dt))
+		elapsed_time = end_dt - self.start_dt
+		print(f'Time Taken: {elapsed_time}')
