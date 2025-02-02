f8fd4891433d09c96f4d10a3e467b5113d48e375
V-Karch <luna.karch05@gmail.com>
2025-02-01 22:11:49 -0500

    black: automatic formatting

--- core/__init__.py
+++ core/__init__.py
@@ -2,10 +2,12 @@
 This LSTM core module provides an implementation of time series prediction using an LSTM approach.
 It is provided as-is with no warranties or support.
 """
+
 __author__ = "Jakob Aungiers"
 __copyright__ = "Jakob Aungiers 2018"
 __version__ = "2.0.0"
 __license__ = "MIT"
 
 import warnings
-warnings.filterwarnings("ignore") # Ignore messy NumPy warnings
+
+warnings.filterwarnings("ignore")  # Ignore messy NumPy warnings
